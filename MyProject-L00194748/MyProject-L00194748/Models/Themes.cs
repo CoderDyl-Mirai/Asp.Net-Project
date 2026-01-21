50d0ef3c558bd1702e47c558c47d3dfff5d51c01
Dylan Greene - STUDENT <l00194748@atu.ie>
2026-01-21 16:28:19 +0000

    First Commit

---
+++ MyProject-L00194748/MyProject-L00194748/Models/Themes.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyProject_L00194748.Models
+{
+    public class Themes
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public List<BookDetails> Books { get; set; }
+    }
+}
